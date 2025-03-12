bd9ab4710c6b265c0955deeebb150ce182502f5e
Taysmar <taysmar@outlook.com>
2025-03-12 10:55:36 -0300

    Alterações na página de carrinho de compras

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Gallery from '../components/Gallery';
--- src/pages/Shopping.js
+++ src/pages/Shopping.js
@@ -11,55 +11,69 @@ export default function Shopping() {
     <>
       <Header />
       <div className="divider-container">
+        <Link to='/'>
       <LuArrowLeft className='arrow-back'/>
+        </Link>
       </div>
 
       <div className='body-container'>
-        <div className='itens-container'>
-          <img></img>
-          <p></p>
-          <p></p>
-        </div>
 
-        <div className='itens-container'>
-          <img></img>
+      <div className='itens-container'>
+      <div className='item-container'>
+          <img alt='item-photo'></img>
           <p>nome: </p>
           <p>quantidade: </p>
         </div>
 
-        <div className='itens-container'>
-          <img></img>
-          <p></p>
-          <p></p>
+        <div className='item-container'>
+          <img alt='item-photo'></img>
+          <p>nome: </p>
+          <p>quantidade: </p>
         </div>
+
+        <div className='item-container'>
+          <img alt='item-photo'></img>
+          <p>nome: </p>
+          <p>quantidade: </p>
+        </div>
+      </div>
 
         <div className='order-resume-container'>
           <p className='resume-title'>Resumo do pedido</p>
           <div className='resume'>
+            <div className='quantity'>
           <p>Quantidade </p>
           <p>3</p>
+            </div>
+            <div className='price'>
           <p>Preço</p>
           <p>R$ 70,00</p>
+            </div>
+            <div className='delivery-tax'>
           <p>Frete</p>
           <p>R$ 9,90</p>
+            </div>
           </div>
-          <div className='divider-t'></div>
+          <div className='divider'></div>
           <div className='total'>
             <p>Total</p>
-            <p>R$ 79,90</p>
-            <p>Em até 2x no cartão</p>
+            <div className='payments-types'>
+            <p className='total-amount'>R$ 79,90</p>
+            <p className='payment-fraction'>Em até 2x no cartão</p>
+            </div>
           </div>
-          <div className='divider-b'></div>
+          <div className='divider'></div>
           <div className='button-container'>
-          <button>Adicionar mais produtos</button>
-          <button>Continuar</button>
+          <button className='add-more-bt'>Adicionar mais produtos</button>
+          <button className='continue-bt'>Continuar</button>
           </div>
         </div>
-        <div className='frete-container'></div>
-        <p className='frete-title'>Calcule o frete</p>
-        <input></input>
-        <button>ok</button>
       </div>
+        <div className='frete-container'>
+        <p className='frete-title'>Calcule o frete</p>
+        <input placeholder='0000 - 000' maxLength={7}></input>
+        <button className='frete-bt'>ok</button>
+        </div>
       <Footer className='footer'/>
     </>
   );
